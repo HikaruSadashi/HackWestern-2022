b5a555196ce05126466b57674d4be80ef4ba6918
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2022-11-19 14:38:31 -0500

    Push landing page to prod

--- SMS/noti.js
+++ SMS/noti.js
@@ -3,6 +3,8 @@
 //cant make this file public because of account SID & etc
 const client = require('twilio')('ACd3f20f3965aa588c0812aea6f9b7b6f1', '683bab841682a15e4712831822c06029');
 
+//check database 
+
 
 client.messages
   .create({
